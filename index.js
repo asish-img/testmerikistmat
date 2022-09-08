a9d2ffb41c0aea866d766a7671888c57e1160daa
asish-img <asishprajapati.img@gmail.com>
2022-09-08 15:01:28 +0530

    kistmat test

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Haye meri kismat ",
+    statuscode: 200,
+  });
+});
+
 app.listen(5822, () => {
   console.log("Server running on 5822");
 });
